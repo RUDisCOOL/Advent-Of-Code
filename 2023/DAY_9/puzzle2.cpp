de4589661a5e4a684644b52009251f8955556964
Rudra Patel <superrudra1601@gmail.com>
2024-11-04 00:59:15 +0530

    2023 DAY9 puzzle 2

---
+++ 2023/DAY_9/puzzle2.cpp
@@ -0,0 +1,55 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+void getHistoryValues(const string &line, vector<int> &history) {
+    istringstream iss(line);
+    int num;
+    while (iss >> num) {
+        history.push_back(num);
+    }
+}
+
+void predictPreviousValue(vector<int> &history) {
+    vector<int> currVec;
+    bool isAllZero = true;
+    for (int i = 1; i < history.size(); i++) {
+        int diff = history[i] - history[i - 1];
+        currVec.push_back(diff);
+        if (diff != 0) {
+            isAllZero = false;
+        }
+    }
+    if (!isAllZero) {
+        predictPreviousValue(currVec);
+    }
+    history.push_back(history[0] - currVec.back());
+}
+
+int main() {
+    ifstream file("./input.txt");
+    if (!file.is_open()) {
+        cerr << "Error opening file!!\n";
+        return 1;
+    }
+    string line;
+    int sum = 0;
+    while (getline(file, line)) {
+        //* MAIN LOGIC OF CODE
+        vector<int> history;
+        getHistoryValues(line, history);
+        predictPreviousValue(history);
+        sum += history.back();
+        //* Debugging Messages
+        // for (const int &num : history) {
+        //     cout << num << " ";
+        // }
+        // cout << endl;
+    }
+    cout << sum;
+
+    file.close();
+    return 0;
+}
