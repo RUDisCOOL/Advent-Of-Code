df2ce55eb06cf3003aedf9a7cef8cd32f0a6a41b
Rudra Patel <superrudra1601@gmail.com>
2024-12-03 17:39:25 +0530

    2024 DAY3 puzzle 2 Optimized

--- 2024/DAY_3/puzzle2.cpp
+++ 2024/DAY_3/puzzle2.cpp
@@ -5,11 +5,6 @@
 #include <vector>
 using namespace std;
 
-struct stringPos {
-    string str;
-    int index;
-};
-
 bool isNumber(const string &str) {
     for (const char &ch : str) {
         if (!isdigit(ch)) {
@@ -31,27 +26,24 @@ int main() {
     static const regex reg("(mul\\(([0-9]{1,3}),([0-9]{1,3})\\))|(don't\\(\\))|(do\\(\\))");
     smatch match;
     int sum = 0;
-    vector<stringPos> stringIndexes;
+    vector<string> stringIndexes;
     while (getline(file, line)) {
-        int positionOffset = 0;
         while (regex_search(line, match, reg)) {
-            positionOffset += match.position() + match[0].length();
             if (match[1].matched) {
-                stringIndexes.push_back({to_string(stoi(match[2]) * stoi(match[3])), positionOffset});
+                stringIndexes.push_back(to_string(stoi(match[2]) * stoi(match[3])));
             } else {
-                stringIndexes.push_back({match[0], positionOffset});
+                stringIndexes.push_back(match[0]);
             }
             line = match.suffix();
         }
     }
     bool flag = true;
-    for (const stringPos &tup : stringIndexes) {
-        // cout << tup.str << " " << tup.index << endl;
-        if (flag && isNumber(tup.str)) {
-            sum += stoi(tup.str);
-        } else if (tup.str == "don't()") {
+    for (const string &tup : stringIndexes) {
+        if (flag && isNumber(tup)) {
+            sum += stoi(tup);
+        } else if (tup == "don't()") {
             flag = false;
-        } else if (tup.str == "do()") {
+        } else if (tup == "do()") {
             flag = true;
         }
     }
